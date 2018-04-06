c3b672aa07d2462018cd7f54ff79a6f0cfd08e6d
Josh Evenson <jevenson@bhtp.com>
2018-04-06 17:37:03 -0500

    added the bootstraps

--- build.js
+++ build.js
@@ -9,6 +9,7 @@ const
     handlebars = require('handlebars'),
     autoprefixer = require('metalsmith-autoprefixer'),
     assets = require('metalsmith-assets'),
+    copy = require('metalsmith-copy-assets-540'),
     browsersync = require('metalsmith-browser-sync'),
     circularJSON = require('circular-json'),
     appConfig = require('./app-config.json');
@@ -27,6 +28,12 @@ const pipeline =
         .use(assets({
             source: 'src/site/assets',
             destination: 'assets'
+        }))
+        .use(copy({
+            src: [
+                'node_modules/bootstrap/dist/css/bootstrap.min.css'
+            ],
+            dest: 'styles'
         }))
         .use(collections({
             pages: {
