83c31625d7f9a07f907618b4558901d3494cbc29
Josh Evenson <jevenson@outlook.com>
2018-12-27 18:41:20 -0600

    format with prettier

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,37 +1,34 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from 'react';
+import { Link } from 'gatsby';
 
 type Props = {
-  siteTitle: string
-}
+    siteTitle: string;
+};
 
 const Header = ({ siteTitle }: Props) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
     <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+        style={{
+            background: 'rebeccapurple',
+            marginBottom: '1.45rem'
+        }}>
+        <div
+            style={{
+                margin: '0 auto',
+                maxWidth: 960,
+                padding: '1.45rem 1.0875rem'
+            }}>
+            <h1 style={{ margin: 0 }}>
+                <Link
+                    to="/"
+                    style={{
+                        color: 'white',
+                        textDecoration: 'none'
+                    }}>
+                    {siteTitle}
+                </Link>
+            </h1>
+        </div>
     </div>
-  </div>
-)
+);
 
-export default Header
+export default Header;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,23 @@
-import React, { Component } from 'react'
-import { Link } from 'gatsby'
-import { css } from 'emotion'
+import React, { Component } from 'react';
+import { Link } from 'gatsby';
+import { css } from 'emotion';
 
-import Layout from '../components/layout'
+import Layout from '../components/layout';
 
 const header = css({
-  color: '#0505',
-})
+    color: '#0505'
+});
 
 class IndexPage extends Component {
-  render() {
-    return (
-      <Layout>
-        <h1 className={header}>Hi people</h1>
-        <p>Welcome to your new Gatsby site.</p>
-        <p>Now go build something great.</p>
-        <Link to="/page-2/">Go to page 2</Link>
-      </Layout>
-    )
-  }
+    render() {
+        return (
+            <Layout>
+                <h1 className={header}>Hi people</h1>
+                <p>Welcome to your new Gatsby site.</p>
+                <p>Now go build something great.</p>
+                <Link to="/page-2/">Go to page 2</Link>
+            </Layout>
+        );
+    }
 }
-export default IndexPage
+export default IndexPage;
