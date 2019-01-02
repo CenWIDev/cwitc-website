a1b1183f053b5bcbae8c20a8431ac150c664c421
Josh Evenson <Joshua.Evenson@bhspecialty.com>
2019-01-02 13:17:29 -0600

    a few package updates to allow for using the rich text field

--- src/components/contentPageLayout.tsx
+++ src/components/contentPageLayout.tsx
@@ -5,27 +5,40 @@ import Layout from './layout';
 
 export default class ContentPageLayout extends Component {
 
+    public props: any;
+
     public render(): ReactNode {
+        const { contentfulContentPageLayout } = this.props.data;
+        const { heading, subheading, body } = contentfulContentPageLayout;
+
         return (
             <Layout>
-                <h1>content page</h1>
+                <h1>{ heading }</h1>
+                <h3>{ subheading }</h3>
+                <div dangerouslySetInnerHTML={{ __html: body.childContentfulRichText.html }} />
             </Layout>
         );
     }
 }
 
-// export const query = graphql`
-//     query PageQuery($slug: String!) {
-//         markdownRemark(frontmatter: {
-//             slug: {
-//                 eq: $slug
-//             }
-//         }) {
-//             html
-//             frontmatter {
-//                 title
-//                 date
-//                 slug
-//             }
-//         }
-//     }`;
+export const query = graphql`
+    query ContentPageQuery($slug: String!) {
+        contentfulContentPageLayout(page: {
+            slug: {
+                eq: $slug
+            }
+        }) {
+            heading
+            subheading
+            body {
+                childContentfulRichText {
+                    html
+                }
+            }
+            page {
+                title
+                socialMediaShareDescription
+                slug
+            }
+        }
+    }`;
