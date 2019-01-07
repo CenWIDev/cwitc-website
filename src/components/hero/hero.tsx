54b65c1bf13f58dec1291898301f86e65ec141c8
Josh Evenson <jevenson@outlook.com>
2019-01-07 07:50:44 -0600

    hero component

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -2,20 +2,30 @@ import React, { Component, ReactNode } from 'react';
 import styled from 'styled-components';
 
 import { Container } from './../layout/container';
-
-
 import { color } from './../../styles/variables'
+
 const Heading = styled.h1`
     color: ${ color.white };
+`;
+
+const Subheading = styled.h3`
+    color: ${ color.white };
+`;
+
+const HeroWrapper = styled(Container)`
+    flex-direction: column;
+    align-items: center;
 `;
 
 export class Hero extends Component {
 
     public render(): ReactNode {
         return (
-            <Container>
+            <HeroWrapper>
                 <Heading>Hero Component</Heading>
-            </Container>
+                <Subheading>Subheading</Subheading>
+
+            </HeroWrapper>
         );
     }
 }
