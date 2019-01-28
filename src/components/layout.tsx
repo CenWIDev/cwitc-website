dd815def06a51dfa52e39af755cc6c8284f10314
Josh Evenson <jevenson@outlook.com>
2019-01-27 19:49:31 -0600

    testing social tags

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,6 +4,7 @@ import { StaticQuery, graphql } from 'gatsby';
 
 import Header from './header/header';
 import Footer, { FooterProps } from './footer/footer';
+import { trimChar } from './../services/text-helper';
 
 // Imports Bootstrap
 import './_vars.scss';
@@ -14,15 +15,17 @@ import 'core-js/es6/number';
 type Props = {
     isHomePage?: boolean;
     className?: string;
+    path: string;
     children: React.ReactNode;
 };
 
-const Layout = ({ isHomePage = false, className, children }: Props) => (
+const Layout = ({ isHomePage = false, className, path, children }: Props) => (
     <StaticQuery
         query={graphql`
             query GlobalSiteSettings {
                 siteSettings: contentfulGlobalSiteSettings {
                     siteName
+                    siteUrl
                     addressLine1
                     addressLine2
                     cityStatePostalCode
@@ -31,10 +34,20 @@ const Layout = ({ isHomePage = false, className, children }: Props) => (
                     twitterUsername
                     linkedInProfileUrl
                     gitHubProfileUrl
+                    headerLogo {
+                        fixed(width: 500) {
+                            src
+                        }
+                    }
+                }
+                hero: contentfulHomePageHero {
+                    description {
+                        description
+                    }
                 }
             }
         `}
-        render={({ siteSettings }) => {
+        render={({ siteSettings, hero }) => {
 
             const footerConfig: FooterProps = {
                 addressLine1: siteSettings.addressLine1,
@@ -48,15 +61,26 @@ const Layout = ({ isHomePage = false, className, children }: Props) => (
                 siteName: siteSettings.siteName
             };
 
+            let canonicalUrl: string = trimChar(siteSettings.siteUrl, '/');
+
+            if (path) {
+                canonicalUrl += `/${ trimChar(path, '/') }`;
+            }
+
             return (
                 <>
                     <Helmet
                         title={`${ siteSettings.siteName }`}
                         meta={[
-                            { name: 'description', content: 'Sample' },
-                            { name: 'keywords', content: 'sample, something' }
+                            { property: 'og:type', content: 'website' },
+                            { property: 'og:url', content: canonicalUrl },
+                            { property: 'twitter:card', content: 'summary' },
+                            { property: 'twitter:site:id', content: siteSettings.twitterUsername },
+                            { property: 'og:image', content: siteSettings.headerLogo.fixed.src },
+                            { property: 'og:description', content: hero.description.description }
                         ]}>
                         <html lang="en" />
+                        <link rel="canonical" href={ canonicalUrl } />
                     </Helmet>
                     <Header useHero={ isHomePage }/>
                     <div className={ className }>
