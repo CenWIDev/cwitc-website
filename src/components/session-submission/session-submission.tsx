e8998fbfe158a48ff0e8da7c9c1dd47f615a8e5b
Kyle Vogt <kylev@bhtp.com>
2019-02-04 17:37:08 -0600

    formatting

--- src/components/session-submission/session-submission.tsx
+++ src/components/session-submission/session-submission.tsx
@@ -98,7 +98,7 @@ export default class SessionSubmission extends Component {
                         <div className="form-row mt-3">
                           <div className="col-sm-12 ">
                             <div className="d-flex justify-content-center">
-                              <button className="btn btn-outline-primary" type="button" onClick={() => arrayHelpers.push(this.buildEmptyPresenter())}>Add Presenter</button>
+                              <button className="btn btn-outline-primary" type="button" onClick={ () => arrayHelpers.push(this.buildEmptyPresenter()) }>Add Presenter</button>
                             </div>
                           </div>
                         </div>
@@ -109,7 +109,7 @@ export default class SessionSubmission extends Component {
                     <div className="col-sm-12 col-md-6">
                       <div className="form-group">
                         <label htmlFor="title">Session Title</label>
-                        <Field className={`form-control ${touched.title && errors.title ? 'is-invalid' : 'is-valid'}`} placeholder="How To Do Amazing Things" name="title" id="title" />
+                        <Field className={ `form-control ${ touched.title && errors.title ? 'is-invalid' : 'is-valid'}` } placeholder="How To Do Amazing Things" name="title" id="title" />
                         <div className="invalid-feedback">
                           <ErrorMessage name="title" />
                         </div>
@@ -127,9 +127,9 @@ export default class SessionSubmission extends Component {
                       <div className="form-group">
                         <label>Target Level</label>
                         {['100', '200', '300', '400'].map((level) => (
-                          <div key={level} className="form-check">
-                            <Field type="radio" className="form-check-input" name="targetLevel" id={`targetLevel${level}`} value={level} />
-                            <label className="form-check-label" htmlFor={`targetLevel${level}`}>{level}</label>
+                          <div key={ level } className="form-check">
+                            <Field type="radio" className="form-check-input" name="targetLevel" id={ `targetLevel${ level}` } value={ level } />
+                            <label className="form-check-label" htmlFor={ `targetLevel${ level}` }>{level}</label>
                           </div>
                         ))}
                       </div>
@@ -138,9 +138,9 @@ export default class SessionSubmission extends Component {
                       <div className="form-check">
                         <label>Are you interested in hosting an Open Lab?</label>
                         {['Yes', 'No', 'Maybe'].map((interestOption) => (
-                          <div key={interestOption} className="form-check">
-                            <Field type="radio" className="form-check-input" name="interestedInOpenLab" id={`interestedInOpenLab${interestOption}`} value={interestOption} />
-                            <label className="form-check-label" htmlFor={`interestedInOpenLab${interestOption}`}>{interestOption}</label>
+                          <div key={ interestOption } className="form-check">
+                            <Field type="radio" className="form-check-input" name="interestedInOpenLab" id={ `interestedInOpenLab${ interestOption}` } value={ interestOption } />
+                            <label className="form-check-label" htmlFor={ `interestedInOpenLab${ interestOption}` }>{interestOption}</label>
                           </div>
                         ))}
                       </div>
@@ -149,7 +149,7 @@ export default class SessionSubmission extends Component {
                   <div className="form-row mt-3">
                     <div className="col-sm-12 ">
                       <div className="d-flex justify-content-center">
-                        <button className="btn btn-outline-primary" type="submit" disabled={isSubmitting}>
+                        <button className="btn btn-outline-primary" type="submit" disabled={ isSubmitting }>
                           Submit
                         </button>
                       </div>
@@ -169,36 +169,36 @@ export default class SessionSubmission extends Component {
       arrayHelpers: ArrayHelpers,
       errors: FormikErrors<Session>,
       touched: FormikTouched<Session>): any {
-    const firstNamePath: string = `presenters.${index}.firstName`;
-    const lastNamePath: string = `presenters.${index}.lastName`;
-    const bioPath: string = `presenters.${index}.bio`;
-    const titlePath: string = `presenters.${index}.title`;
-    const companyPath: string = `presenters.${index}.company`;
-    const phoneNumberPath: string = `presenters.${index}.phoneNumber`;
-    const emailPath: string = `presenters.${index}.email`;
+    const firstNamePath: string = `presenters.${ index }.firstName`;
+    const lastNamePath: string = `presenters.${ index }.lastName`;
+    const bioPath: string = `presenters.${ index }.bio`;
+    const titlePath: string = `presenters.${ index }.title`;
+    const companyPath: string = `presenters.${ index }.company`;
+    const phoneNumberPath: string = `presenters.${ index }.phoneNumber`;
+    const emailPath: string = `presenters.${ index }.email`;
 
     return (
-      <React.Fragment key={index}>
+      <React.Fragment key={ index }>
         <div className="form-row">
           <div className="col-sm-12 col-md-6">
             <div className="form-group">
-              <label htmlFor={firstNamePath}>First Name</label>
+              <label htmlFor={ firstNamePath }>First Name</label>
               <Field
-                className={`form-control ${getValidationClass(firstNamePath)}`}
-                placeholder="John" name={firstNamePath} id={firstNamePath} />
+                className={ `form-control ${ getValidationClass(firstNamePath) }`}
+                placeholder="John" name={ firstNamePath } id={ firstNamePath } />
               <div className="invalid-feedback">
-                <ErrorMessage name={firstNamePath} />
+                <ErrorMessage name={ firstNamePath } />
               </div>
             </div>
           </div>
           <div className="col-sm-12 col-md-6">
             <div className="form-group">
-              <label htmlFor={lastNamePath}>Last Name</label>
+              <label htmlFor={ lastNamePath }>Last Name</label>
               <Field
-                className={`form-control ${getValidationClass(lastNamePath)}`}
-                placeholder="Doe" name={lastNamePath} id={lastNamePath} />
+                className={ `form-control ${ getValidationClass(lastNamePath) }`}
+                placeholder="Doe" name={ lastNamePath } id={ lastNamePath } />
               <div className="invalid-feedback">
-                <ErrorMessage name={lastNamePath} />
+                <ErrorMessage name={ lastNamePath } />
               </div>
             </div>
           </div>
@@ -206,43 +206,46 @@ export default class SessionSubmission extends Component {
         <div className="form-row">
           <div className="col-sm-12">
             <div className="form-group">
-              <label htmlFor={bioPath}>Bio</label>
-              <Field component="textarea" className="form-control" placeholder="I am a blank at blank working mostly with...." name={bioPath} id={bioPath} />
+              <label htmlFor={ bioPath }>Bio</label>
+              <Field component="textarea" className="form-control" placeholder="I am a blank at blank working mostly with...." name={ bioPath } id={ bioPath } />
+              <div className="invalid-feedback">
+                <ErrorMessage name={ bioPath } />
+              </div>
             </div>
           </div>
         </div>
         <div className="form-row">
           <div className="col-sm-12 col-md-6">
             <div className="form-group">
-              <label htmlFor={titlePath}>Title</label>
-              <Field className="form-control" placeholder="Lead Doer of Stuff" name={titlePath} id={titlePath} />
+              <label htmlFor={ titlePath }>Title</label>
+              <Field className="form-control" placeholder="Lead Doer of Stuff" name={ titlePath } id={ titlePath } />
             </div>
           </div>
           <div className="col-sm-12 col-md-6">
             <div className="form-group">
-              <label htmlFor={companyPath}>Company</label>
-              <Field className="form-control" placeholder="Some Startup" name={companyPath} id={companyPath} />
+              <label htmlFor={ companyPath }>Company</label>
+              <Field className="form-control" placeholder="Some Startup" name={ companyPath } id={ companyPath } />
             </div>
           </div>
         </div>
         <div className="form-row">
           <div className="col-sm-12 col-md-6">
             <div className="form-group">
-              <label htmlFor={phoneNumberPath}>Phone Number</label>
-              <Field type="phone" className="form-control" placeholder="715-123-4567" name={phoneNumberPath} id={phoneNumberPath} />
+              <label htmlFor={ phoneNumberPath }>Phone Number</label>
+              <Field type="phone" className="form-control" placeholder="715-123-4567" name={ phoneNumberPath } id={ phoneNumberPath } />
             </div>
           </div>
           <div className="col-sm-12 col-md-6">
             <div className="form-group">
-              <label htmlFor={emailPath}>Email</label>
-              <Field type="email" className="form-control" placeholder="email@gmail.com" name={emailPath} id={emailPath} />
+              <label htmlFor={ emailPath }>Email</label>
+              <Field type="email" className="form-control" placeholder="email@gmail.com" name={ emailPath } id={ emailPath } />
             </div>
           </div>
         </div>
         <div className="form-row">
           <div className="col-sm-12 ">
             <div className="d-flex justify-content-end">
-              <button className="btn btn-outline-primary" type="button" onClick={() => arrayHelpers.remove(index)}>Remove Presenter</button>
+              <button className="btn btn-outline-primary" type="button" onClick={ () => arrayHelpers.remove(index) }>Remove Presenter</button>
             </div>
           </div>
         </div>
@@ -253,7 +256,7 @@ export default class SessionSubmission extends Component {
       if (getIn(touched, path.split('.'))) {
         if (getIn(errors, path.split('.'))) {
           return 'is-invalid';
-        } else {
+         } else {
           return 'is-valid'
         }
       }
