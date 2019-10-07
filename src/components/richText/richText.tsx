51ca5117c5c073073d06bd2a1f0ec09909454d92
Josh Evenson <jevenson@bhtp.com>
2019-10-07 10:02:53 -0500

    add support for lunch venues

--- src/components/richText/richText.tsx
+++ src/components/richText/richText.tsx
@@ -39,22 +39,43 @@ const externalHyperlinkRenderer = (node: any): ReactNode => {
     return <a href={` ${ location } `} target="_blank" rel="noopener">{ text }</a>;
 };
 
+const inlineEmbeddedEntryRenderer = (node: any): ReactNode => {
+    try {
+        if (node.data.target.sys.contentType.sys.id === 'partner' && node.data.target.fields.partnerType['en-US'] === 'Lunch Venue') {
+            return (
+                <>
+                    <a href={ node.data.target.fields.siteUrl['en-US'] } target="_blank" rel="noopener">{ node.data.target.fields.name['en-US']}</a> <br/>
+                    <span>{ newLineFormatter(node.data.target.fields.address['en-US']) }</span>
+                </>
+            );
+        }
+    }
+    catch (err) {
+        console.warn('Renderer has not been implemented for inline entry', err);
+    }
+
+    return null;
+};
+
 const RichText = ({ richText }: { richText: Document }): React.ReactElement<any> => {
     const options: Options = {
         renderNode: {
             [INLINES.ENTRY_HYPERLINK]: entryHyperlinkRenderer,
             [INLINES.ASSET_HYPERLINK]: assetHyperlinkRenderer,
-            [INLINES.HYPERLINK]: externalHyperlinkRenderer
+            [INLINES.HYPERLINK]: externalHyperlinkRenderer,
+            [INLINES.EMBEDDED_ENTRY]: inlineEmbeddedEntryRenderer
         },
         // https://github.com/contentful/rich-text/tree/master/packages/rich-text-react-renderer
-        renderText: text => {
-            return text.split('\n').reduce((children: ReactNode, textSegment: string, index: number) => {
-                return [...children, index > 0 && <br key={ index } />, textSegment];
-            }, []);
-        }
+        renderText: newLineFormatter
     };
 
     return documentToReactComponents(richText, options) as React.ReactElement<any>;
 };
 
 export default RichText;
+
+const newLineFormatter = (text: string): ReactNode => {
+    return text.split('\n').reduce((prev: ReactNode, curr: string, index: number) => {
+        return [...prev, index > 0 && <br key={ index } />, curr];
+    }, []);
+};
