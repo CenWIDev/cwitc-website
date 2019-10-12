5633b688a17c589b30f13ddcc68d4de0c2fa85f4
Josh Evenson <jevenson@bhtp.com>
2019-10-11 20:04:48 -0500

    remove serivce worker plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,7 +55,8 @@ module.exports = {
         }
       }
     },
-    'gatsby-plugin-offline',
+    // 'gatsby-plugin-offline',
+    'gatsby-plugin-remove-serviceworker',
     'gatsby-plugin-typescript',
     'gatsby-transformer-remark',
     'gatsby-plugin-sass',
