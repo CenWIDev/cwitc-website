ee830bdfe0e651c3069385f92f1209cbc22fac67
Jack Smith <sythasfaroth@gmail.com>
2019-10-19 14:31:54 -0500

    implemented client side 404

---
+++ src/components/not-found/notFound.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Icon from '../icon/icon';
+// import { RouteComponentProps } from '@reach/router';
+
+const NotFound = () => (
+    <div className="container mt-4">
+        <div className="row">
+            <div className="col d-flex flex-column align-items-center">
+                <Icon name="zap" />
+                <h1>Whoops! 404</h1>
+                <p>This page doesn't exist</p>
+            </div>
+        </div>
+    </div>
+);
+
+NotFound.defaultProps = {
+    default: true
+};
+
+export default NotFound;
--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -4,16 +4,18 @@ import Layout from '../components/layout';
 import { PrivateRoute } from './../components/privateRoute/privateRoute';
 import Profile from '../components/profile/profile';
 import Login from '../components/login/login';
+import NotFound from '../components/not-found/notFound';
 // import SessionSubmission from '../components/session-submission/session-submission';
 
 const App = (props: any) => (
-  <Layout path={ props.location.pathname }>
-    <Router>
-      <PrivateRoute path="/app/profile" component={ Profile }/>
-      {/* <PrivateRoute path="/app/submit-session" component={ SessionSubmission }/> */}
-      <Login path="/app/log-in" />
-    </Router>
-  </Layout>
+    <Layout path={props.location.pathname}>
+        <Router>
+            <PrivateRoute path="/app/profile" component={Profile} />
+            {/* <PrivateRoute path="/app/submit-session" component={ SessionSubmission }/> */}
+            <Login path="/app/log-in" />
+            <NotFound default />
+        </Router>
+    </Layout>
 );
 
 export default App;
