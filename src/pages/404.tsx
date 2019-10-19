6b450c318ec1d102938085935c2a973fe4fc3242
sythas <sythas@users.noreply.github.com>
2019-10-19 14:48:33 -0500

    implemented client side 404 (#75)
    * implemented client side 404
    
    * removed comment
    
    * oops
    
    * Removed extra comment.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
 import Layout from '../components/layout';
-import Icon from './../components/icon/icon';
+import NotFound from '../components/not-found/notFound';
 
 const NotFoundPage = () => (
     <Layout path="/404">
-        <div className="container mt-4">
-            <div className="row">
-                <div className="col d-flex flex-column align-items-center">
-                    <Icon name="zap" />
-                    <h1>Whoops! 404</h1>
-                    <p>This page doesn't exist</p>
-                </div>
-            </div>
-        </div>
+        <NotFound />
     </Layout>
 );
 
