42b0325688b4a10d17ffebfc3867dcc0e9b6fce6
sythas <sythas@users.noreply.github.com>
2019-10-19 14:44:05 -0500

    removed comment

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -5,7 +5,6 @@ import { PrivateRoute } from './../components/privateRoute/privateRoute';
 import Profile from '../components/profile/profile';
 import Login from '../components/login/login';
 import NotFound from '../components/not-found/notFound';
-// import SessionSubmission from '../components/session-submission/session-submission';
 
 const App = (props: any) => (
     <Layout path={props.location.pathname}>
