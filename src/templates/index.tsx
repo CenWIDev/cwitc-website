10356a47a1155380fa6b88beee7864c82e51a1aa
Josh Evenson <jevenson@bhtp.com>
2020-07-26 15:30:57 -0500

    added support for page entries

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -141,7 +141,7 @@ const IndexPage = ({ data }: any) => {
                 </div>
                 <div className="row justify-content-center">
                     <div className="col-10">
-                        <div className="row justify-content-center align-items-center">
+                        <div className="row justify-content-center align-items-top">
                         {
                             partners.map(({ name, siteUrl, logo, sponsorshipLevel }: any, index: number) => (
                                 <div className="sponsor-card-container col-12 col-md-6 col-lg-4 mb-3" key={ index }>
