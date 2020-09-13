670dc6e55624363fad466f879bc937899a51e9cf
Josh Evenson <jevenson@bhtp.com>
2020-09-12 20:16:11 -0500

    remove embedded entry rederer for job listing

--- src/components/richText/richText.tsx
+++ src/components/richText/richText.tsx
@@ -1,8 +1,7 @@
 import React, { ReactNode } from 'react';
 import { Link } from 'gatsby';
-import { Document, INLINES, BLOCKS } from '@contentful/rich-text-types';
+import { Document, INLINES } from '@contentful/rich-text-types';
 import { documentToReactComponents, Options } from '@contentful/rich-text-react-renderer';
-import { JobListing, JobListingProps } from './../job-listing/job-listing';
 
 const englishUsLocale: string = 'en-US';
 
@@ -61,41 +60,13 @@ const inlineEmbeddedEntryRenderer = (node: any): ReactNode => {
     return null;
 };
 
-const blockEmbeddedEntryRenderer = (node: any): ReactNode => {
-    try {
-        const embeddedEntryType = node.data.target.sys.contentType.sys.id;
-
-        if (embeddedEntryType === 'jobListing') {
-            const { fields } = node.data.target;
-
-            const listing: JobListingProps = {
-                logoUrl: fields.company[englishUsLocale].fields.logo[englishUsLocale].fields.file[englishUsLocale].url,
-                companyName: fields.company[englishUsLocale].fields.name[englishUsLocale],
-                title: fields.title[englishUsLocale],
-                applicationLink: fields.applicationLink[englishUsLocale],
-                description: fields.description[englishUsLocale]
-            };
-
-            return (
-                <JobListing {...listing} />
-            );
-        }
-    }
-    catch (err) {
-        console.warn('Renderer has not been implemented for block entry', err);
-    }
-
-    return null;
-};
-
 const RichText = ({ richText }: { richText: Document }): React.ReactElement<any> => {
     const options: Options = {
         renderNode: {
             [INLINES.ENTRY_HYPERLINK]: entryHyperlinkRenderer,
             [INLINES.ASSET_HYPERLINK]: assetHyperlinkRenderer,
             [INLINES.HYPERLINK]: externalHyperlinkRenderer,
-            [INLINES.EMBEDDED_ENTRY]: inlineEmbeddedEntryRenderer,
-            [BLOCKS.EMBEDDED_ENTRY]: blockEmbeddedEntryRenderer
+            [INLINES.EMBEDDED_ENTRY]: inlineEmbeddedEntryRenderer
         },
         // https://github.com/contentful/rich-text/tree/master/packages/rich-text-react-renderer
         renderText: newLineFormatter
