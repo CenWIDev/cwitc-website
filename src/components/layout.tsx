bf9e8939e28be51c28d57430ce94200151467dea
Josh Evenson <jevenson@outlook.com>
2021-02-18 17:13:20 -0600

    Remove whova banner for now

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -104,10 +104,12 @@ const Layout = ({ isHomePage = false, className, path, children }: Props) => (
                         <html lang="en" />
                         <link rel="canonical" href={ canonicalUrl } />
                     </Helmet>
+                    {/*
                     <div className="alert alert-secondary m-0 text-center d-flex flex-wrap flex-md-nowrap justify-content-center align-items-center" role="alert">
                         <span>We're using Whova to faciliate our online conference! Head on over to the website to participate or download the app!</span>
                         <a href="https://whova.com/portal/cwic_202008/" target="_blank" rel="noopener noreferer" className="btn btn-secondary ml-md-3 mt-3 mt-md-0">Go to Whova</a>
                     </div>
+                    */}
                     <Header useHero={ isHomePage }/>
                     <div className={ `main ${ className }` }>
                         { children }
